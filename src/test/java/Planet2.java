e639c085e96b41aa61500997d26cd662f7a8e449
moqaddas <moqaddasr@gmial.com>
2024-11-17 22:39:09 -0800

    Planet 2 is added

---
+++ src/test/java/Planet2.java
@@ -0,0 +1,6 @@
+public class Planet2 {
+    String Name;
+    String Color;
+    String Size;
+    String Temperature;
+}
--- src/test/java/SolarSystem.java
+++ src/test/java/SolarSystem.java
@@ -12,6 +12,13 @@ public class SolarSystem {
         fs.Size = "Small";
         fs.Temperature = "Warm";
         System.out.println("Sun: " + fs.Color + " " + fs.Size + " " + fs.Temperature);
+
+        Planet2 Dracula = new Planet2();
+        Dracula.Color = "Red";
+        Dracula.Size = "Medium";
+        Dracula.Temperature = "Hot";
+        System.out.println("Planet: " + Dracula.Color + " " + Dracula.Size + " " + Dracula.Temperature);
+
     }
 
 }
